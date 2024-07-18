3c50a110f74050378bfea142b22254f4f02f7bce
GenesisMay <genesisdayanamayanzatonato@gmail.com>
2024-07-18 09:24:36 -0500

    prueba

---
+++ js/lista.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textoTarea = document.getElementById('texto-tarea');
+    const añadirBtn = document.getElementById('añadir');
+    const listaTarea = document.getElementById('lista-tarea');
+
+    añadirBtn.addEventListener('click', añadirTarea);
+
+    function añadirTarea() {
+        const tareaTexto = textoTarea.value.trim();
+        if (tareaTexto === '') {
+            alert('Por favor ingrese una tarea');
+            return;
+        }
+
+        const li = document.createElement('li');
+        li.textContent = tareaTexto;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Eliminar';
+        deleteBtn.className = 'deleteBtn';
+        deleteBtn.addEventListener('click', () => {
+            listaTarea.removeChild(li);
+        });
+
+        li.addEventListener('dblclick', () => {
+            li.classList.toggle('completed');
+        });
+
+        li.appendChild(deleteBtn);
+        listaTarea.appendChild(li);
+        textoTarea.value = '';
+    }
+});
